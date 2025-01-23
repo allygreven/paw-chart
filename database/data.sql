6e7a2556984a415cf357be7b931630cb1e3642ec
Ally Greven <allysongreven@gmail.com>
2025-01-23 00:47:48 +0000

    initial commit

--- database/data.sql
+++ database/data.sql
@@ -1,11 +1,9 @@
--- Use SQL insert statements to add any
--- starting/dummy data to your database tables
+insert into "immunizations"
+  ("name", "date")
+  values ('Rabies', '2020-02-07'),
+  ('Parvovirus', '2020-02-14');
 
--- EXAMPLE:
-
---  insert into "todos"
---    ("task", "isCompleted")
---    values
---      ('Learn to code', false),
---      ('Build projects', false),
---      ('Get a job', false);
+insert into "medications"
+  ("name", "dose")
+  values ('Tramadol', '5 mg/kg'),
+  ('Prednisone', '0.5 mg/kg')
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,64 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+
+CREATE TABLE "users" (
+  "userId" serial PRIMARY KEY,
+  "username" text,
+  "hashedPassword" integer
+);
+
+CREATE TABLE "pets" (
+  "petId" serial PRIMARY KEY,
+  "name" text,
+  "type" text,
+  "age" integer,
+  "userId" integer
+);
+
+CREATE TABLE "medications" (
+  "medId" serial PRIMARY KEY,
+  "name" text,
+  "dose" text,
+  "petId" integer
+);
+
+CREATE TABLE "immunizations" (
+  "immunizationId" serial PRIMARY KEY,
+  "name" text,
+  "date" date,
+  "petId" integer
+);
+
+CREATE TABLE "symptomChecker" (
+  "symptomId" serial PRIMARY KEY,
+  "name" text,
+  "diagnosis" text,
+  "petId" integer
+);
+
+CREATE TABLE "interactions" (
+  "interactionId" serial PRIMARY KEY,
+  "risk" text,
+  "description" text,
+  "petId" integer,
+  "medId1" integer,
+  "medId2" integer
+);
+
+COMMENT ON COLUMN "pets"."type" IS 'values: dog, cat';
+
+ALTER TABLE "pets" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
+
+ALTER TABLE "medications" ADD FOREIGN KEY ("petId") REFERENCES "pets" ("petId");
+
+ALTER TABLE "immunizations" ADD FOREIGN KEY ("petId") REFERENCES "pets" ("petId");
+
+ALTER TABLE "symptomChecker" ADD FOREIGN KEY ("petId") REFERENCES "pets" ("petId");
+
+ALTER TABLE "interactions" ADD FOREIGN KEY ("petId") REFERENCES "pets" ("petId");
+
+ALTER TABLE "interactions" ADD FOREIGN KEY ("medId1") REFERENCES "medications" ("medId");
+
+ALTER TABLE "interactions" ADD FOREIGN KEY ("medId2") REFERENCES "medications" ("medId");
